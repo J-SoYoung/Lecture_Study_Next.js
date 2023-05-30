541bbedb2a542886abcd1a2127426c984d2b673b
thdud <thgml1444@naver.com>
2023-05-30 18:42:29 +0900

    [ ADD ] next.js프로젝트의 컴포넌트를 생성하라

--- next-start/app/cart/page.js
+++ next-start/app/cart/page.js
@@ -1,17 +1,27 @@
+import { age } from "./data";
+import { name } from "./data";
+import Test from "./test";
+
 export default function Cart() {
   return (
     <div>
-      <h1 className="title">Cart</h1>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <h1 className="title">
+        Cart - {name},{age}
+      </h1>
+      <Test />
+      <CartItem />
+      <CartItem />
+      <CartItem />
+    </div>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
     </div>
   );
 }
