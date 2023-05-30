b821265550ffb3d7f45c52f6838c20cbdcbd6a18
thdud <thgml1444@naver.com>
2023-05-30 17:31:31 +0900

    [ ADD ] next.js에서 라우팅을 구현하라

---
+++ next-start/app/list/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function List() {
+  return (
+    <>
+      <div className="food-box">
+        <h1>상품목록</h1>
+        <div className="food">
+          <h4>상품1 : 4000원</h4>
+        </div>
+        <div className="food">
+          <h4>상품2 : 5000원</h4>
+        </div>
+      </div>
+    </>
+  );
+}
