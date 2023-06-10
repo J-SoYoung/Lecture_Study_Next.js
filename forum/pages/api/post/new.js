4d6ef8456670cec2b5196240747378b8a95121b7
thdud <thgml1444@naver.com>
2023-06-10 23:03:05 +0900

    [ forum-EDIT ] 매개변수 데이터 코드수정

--- forum/pages/api/post/new.js
+++ forum/pages/api/post/new.js
@@ -1,5 +1,4 @@
 import { connectDB } from "@/util/database";
-import { NextResponse } from "next/server";
 
 export default async function handler(req, res) {
   // console.log(req.body);
@@ -11,8 +10,8 @@ export default async function handler(req, res) {
       const client = await connectDB;
       const db = client.db("forum");
       const collection = await db.collection("post").insertOne(req.body);
-      // return res.status(200);
-      return res.redirect(302, "/list");
+      return res.status(200).json("글작성 성공");
+      // return res.redirect(302, "/list");
     } catch (error) {
       return res.status(500).json(error);
     }
