c1a3117ddbaa0c0c4413ed981a9ab2abd342f45c
thdud <thgml1444@naver.com>
2023-06-11 19:21:49 +0900

    [ forum-ADD ] 에러 페이지를 만들어 사용자의 입력 오류 발생시 에러 페이지로 이동

--- forum/pages/api/post/new.js
+++ forum/pages/api/post/new.js
@@ -4,14 +4,15 @@ export default async function handler(req, res) {
   // console.log(req.body);
   if (req.method == "POST") {
     if (req.body.title === "" || req.body.content === "") {
-      return res.status(500).json("빈칸을 채워주세요");
+      // return res.status(500).json("빈칸을 채워주세요");
+      return res.status(500).redirect(302, "/error");
     }
     try {
       const client = await connectDB;
       const db = client.db("forum");
       const collection = await db.collection("post").insertOne(req.body);
-      return res.status(200).json("글작성 성공");
-      // return res.redirect(302, "/list");
+      // return res.status(200).json("글작성 성공");
+      return res.redirect(302, "/");
     } catch (error) {
       return res.status(500).json(error);
     }
