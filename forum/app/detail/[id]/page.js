1ba8a20a9edd9f9c88b86690037470db1b02d9d4
thdud <thgml1444@naver.com>
2023-06-11 19:20:40 +0900

    [ forum-ADD ] 글 수정 기능을 완성하라

--- forum/app/detail/[id]/page.js
+++ forum/app/detail/[id]/page.js
@@ -1,23 +1,21 @@
-import DetailLink from "@/app/list/DetailLink";
+import DetailLink from "../DetailLink";
 import { connectDB } from "@/util/database.js";
 import { ObjectId } from "mongodb";
 
 export default async function Detail(props) {
-  console.log(props.params.id);
   let db = (await connectDB).db("forum");
   let result = await db
     .collection("post")
     .findOne({ _id: new ObjectId(props.params.id) });
+
   return (
-    <>
+    <div className="detail-box">
+      <h2>상세페이지</h2>
       <div>
-        <h4>상세페이지</h4>
         <p>{result.title}</p>
         <p>{result.content}</p>
       </div>
-      <DetailLink>
-        <p>이전페이지</p>
-      </DetailLink>
-    </>
+      <DetailLink> 메인으로 이동 </DetailLink>
+    </div>
   );
 }
