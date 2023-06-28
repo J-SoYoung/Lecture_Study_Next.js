34a757a10a0cc81a9f77d431608d93498381f525
thdud <thgml1444@naver.com>
2023-06-28 10:17:05 +0900

    [ forum-ADD ] aws-s3를 이용한 이미지업로드

--- 01_forum/app/write/page.js
+++ 01_forum/app/write/page.js
@@ -1,7 +1,10 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import LinkButton from "../components/LinkButton";
 
 export default function Write() {
+  const [imgSrc, setImgSrc] = useState("");
+
   return (
     <div className="write-box">
       <div className="form-box">
@@ -9,6 +12,39 @@ export default function Write() {
         <form action="/api/post/new" method="POST">
           <input name="title" placeholder="글 제목을 입력하세요" />
           <input name="content" placeholder="내용을 입력하세요 " />
+          <input
+            type="file"
+            accept="image"
+            onChange={async (e) => {
+              const file = e.target.files[0];
+              const filename = encodeURIComponent(file.name);
+
+              let res = await fetch(`api/post/image?file=${filename}`);
+              res = await res.json();
+              console.log("res-", res);
+              console.log(res.fields);
+              //S3 업로드
+              const formData = new FormData();
+              Object.entries({ ...res.fields, file }).forEach(
+                ([key, value]) => {
+                  formData.append(key, value);
+                }
+              );
+              let imageUpload = await fetch(res.url, {
+                method: "POST",
+                body: formData,
+              });
+              console.log(imageUpload);
+
+              if (imageUpload.ok) {
+                setImgSrc(`${imageUpload.url}/${filename}`);
+                console.log(imgSrc);
+              } else {
+                console.log("실패");
+              }
+            }}
+          />
+          <img src={imgSrc} />
           <button type="submit">글 작성</button>
         </form>
       </div>
