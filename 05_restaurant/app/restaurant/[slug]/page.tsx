6320f598bb61b31b348cdb756af965934ea9fdee
thdud <thgml1444@naver.com>
2023-08-15 01:52:16 +0900

    [ res-component-ADD ] 하나의 페이지를 여러 컴포넌트로 쪼개라

--- 05_restaurant/app/restaurant/[slug]/page.tsx
+++ 05_restaurant/app/restaurant/[slug]/page.tsx
@@ -1,6 +1,3 @@
-import NavBar from "@/app/components/NavBar";
-import Link from "next/link";
-import DetailHeader from "./components/DetailHeader";
 import DetailNavBar from "./components/DetailNavBar";
 import Title from "./components/Title";
 import Rating from "./components/Rating";
@@ -11,21 +8,16 @@ import ReservationCard from "./components/ReservationCard";
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <DetailHeader/>
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <DetailNavBar/>
-            <Title/>
-            <Rating/>
-            <Description/>
-            <DetailImages/>
-            <Review/>
-          </div>
-          <ReservationCard/>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <DetailNavBar/>
+        <Title/>
+        <Rating/>
+        <Description/>
+        <DetailImages/>
+        <Review/>
+      </div>
+      <ReservationCard/>
+    </>
   );
 }
--- 05_restaurant/app/search/components/SearchHeader.tsx
+++ 05_restaurant/app/search/components/SearchHeader.tsx
@@ -1,16 +1,9 @@
-export default function Header(){
+import SearchBar from "@/app/components/SearchBar";
+
+export default function SearchHeader(){
   return (
     <div className="bg-gradient-to-r to-[#F0EDCC] from-[#02343F] p-2">
-      <div className="text-left text-lg py-3 m-auto flex justify-center">
-        <input
-          className="rounded  mr-3 p-2 w-[450px]"
-          type="text"
-          placeholder="State, city or town"
-        />
-        <button className="rounded bg-red-600 px-9 py-2 text-white">
-          Let's go
-        </button>
-      </div>
+      <SearchBar/>
     </div>
   )
 }
